8a14cb65d6333cae83aa5161a4bad6aaf6a7302c
h <huihuilg@outlook.com>
2020-02-12 20:37:20 +0800

    聊天

--- application/common/command/socket.php
+++ application/common/command/socket.php
@@ -8,21 +8,12 @@ class Socket
     public function __construct()
     {
         $this->server = new Swoole\WebSocket\Server("0.0.0.0", 9501);
+        $this->server->set(['worker_num'=>2,'dispatch_mode'=>5]);
         $this->server->on('open', [$this, 'onOpen']);
         //监听接收消息事件
         $this->server->on('message', [$this, 'onMessage']);
         //监听关闭事件
         $this->server->on('close', [$this, 'onClose']);
-        $this->server->on('request', function ($request, $response) {
-            // 接收http请求从get获取message参数的值，给用户推送
-            // $this->server->connections 遍历所有websocket连接用户的fd，给所有用户推送
-            foreach ($this->server->connections as $fd) {
-                // 需要先判断是否是正确的websocket连接，否则有可能会push失败
-                if ($this->server->isEstablished($fd)) {
-                    $this->server->push($fd, $request->get['message']);
-                }
-            }
-        });
         $this->server->start();
     }
 
@@ -32,10 +23,10 @@ class Socket
     public function onOpen($server, $request)
     {
         echo $request->fd . '连接了' . PHP_EOL;//打印到我们终端
-        var_export($request);
+//        var_export($request);
         foreach ($this->server->connections as $fd) {
             // 需要先判断是否是正确的websocket连接，否则有可能会push失败
-            $server->push($fd, '有新人进入聊天室');
+            $server->push($fd, json_encode(['name'=>'','msg'=>'有新人加入聊天室']));
         }
     }
 
@@ -48,10 +39,20 @@ class Socket
     {
         echo $frame->fd . '来了，说：' . $frame->data . PHP_EOL;//打印到我们终端
 //        var_export($this->server->getClientList(0,5));
-//        var_export($this->server->connections);
-        foreach ($this->server->connections as $fd) {
-            // 需要先判断是否是正确的websocket连接，否则有可能会push失败
-            $server->push($fd, $frame->data);
+        $data = json_decode($frame->data);
+        switch ($data->type) {
+            case 'bind':
+//                $server->bind($frame->fd, $frame->fd);
+//                var_export($server->getClientInfo($frame->fd));
+                break;
+            case 'say':
+                foreach ($this->server->connections as $fd) {
+                    // 需要先判断是否是正确的websocket连接，否则有可能会push失败
+                    if ($server->isEstablished($fd)) {
+                        $server->push($fd, json_encode(['name'=>$data->name,'msg'=>$data->msg]));
+                    }
+                }
+                break;
         }
     }
 
