7135033117455880e038c91ea6a686f5ae278569
h <huihuilg@outlook.com>
2020-04-04 23:40:10 +0800

    qq

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,18 +6,10 @@ use app\admin\model\Like;
 use app\admin\model\User;
 use think\Controller;
 use think\Request;
+use QqLogin;
 
 class Index extends Controller
 {
-
-    const VERSION = "2.0";
-    const GET_AUTH_CODE_URL = "https://graph.qq.com/oauth2.0/authorize";
-    const GET_ACCESS_TOKEN_URL = "https://graph.qq.com/oauth2.0/token";
-    const GET_OPENID_URL = "https://graph.qq.com/oauth2.0/me";
-    const USER_INFO = 'https://graph.qq.com/user/get_user_info?access_token=%s&oauth_consumer_key=%s&openid=%s';
-    private $APP_ID = 101544609;
-    private $APP_Key = '778dcbddc5147be3a33cc1f17a290d6d';
-    private $callback = 'http://sunny.huihuil.cn/index/index/qqaction';
 
 //    protected $middleware = [
 //        'Auth' 	=> ['except' 	=> ['index'] ],
@@ -30,13 +22,14 @@ class Index extends Controller
     }
 
     /**
-     * qq登录回调
+     * qq登录
      */
     public function qqLogin()
     {
-        $appid = $this->APP_ID;
-        $callback = $this->callback;
-        $scope = 'get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr';
+        $qqLogin = new QqLogin();
+        $appid = $qqLogin->APP_ID;
+        $callback = $qqLogin->callback;
+        $scope = '';
         //-------生成唯一随机串防CSRF攻击
         $state = md5(uniqid(rand(), TRUE));
         session('state',$state) ;
@@ -48,28 +41,31 @@ class Index extends Controller
             "state" => $state,
             "scope" => $scope
         );
-        $login_url =  $this->combineURL(self::GET_AUTH_CODE_URL, $keysArr);
+        $login_url =  $qqLogin->combineURL(QqLogin::GET_AUTH_CODE_URL, $keysArr);
         header("Location:$login_url");
     }
+    /**
+     * qq登录回调url
+     */
     public function qqaction(Request $request)
     {
+        $qqLogin = new QqLogin();
         $state = session('state') ;;
         //--------验证state防止CSRF攻击
         if(!$state || $request->get('state') != $state){
             echo 'state 验证错误';exit();
         }
-
         //-------请求参数列表
         $keysArr = array(
             "grant_type" => "authorization_code",
-            "client_id" => $this->APP_ID,
-            "redirect_uri" => urlencode($this->callback),
-            "client_secret" => $this->APP_Key,
-            "code" => $_GET['code']
+            "client_id" => $qqLogin->APP_ID,
+            "redirect_uri" => urlencode($qqLogin->callback),
+            "client_secret" => $qqLogin->APP_Key,
+            "code" => $request->get('code')
         );
         //------构造请求access_token的url
-        $token_url = $this->combineURL(self::GET_ACCESS_TOKEN_URL, $keysArr);
-        $response = $this->get_contents($token_url);
+        $token_url = $qqLogin->combineURL(QqLogin::GET_ACCESS_TOKEN_URL, $keysArr);
+        $response = $qqLogin->get_contents($token_url);
         if(strpos($response, "callback") !== false){
 
             $lpos = strpos($response, "(");
@@ -81,15 +77,11 @@ class Index extends Controller
                 echo '错误码：'.$msg->error.'错误描述：'.$msg->error_description;exit();
             }
         }
-        echo $response;
         $params = array();
         parse_str($response, $params);
-//        session('access_token',$params["access_token"]) ;
-//        echo 'access_token是：'.$params["access_token"];
-        $openId = $this->get_openid();
-        $url = sprintf(self::USER_INFO,$params["access_token"],$this->APP_ID,$openId);
+        $openId = $qqLogin->get_openid();
+        $url = sprintf(QqLogin::USER_INFO,$params["access_token"],$qqLogin->APP_ID,$openId);
         $result = json_decode($this->get_contents($url),1);
-
         $user = new User();
         $isExist = $user->where(['user_name'=>$result['nickname']])->find();
         if($isExist){
@@ -106,87 +98,19 @@ class Index extends Controller
             $user->qq_openid = $openId;
             $user->save();
             cookie('user_name',$user->user_name);
-//            cookie('uid',$isLogin->id);
             session('uid',$user->id);
             $this->redirect('/');
         }
-//        dump($result);
-    }
-    public function get_openid(){
-
-        //-------请求参数列表
-        $keysArr = array(
-            "access_token" => session('access_token')
-        );
-
-        $graph_url = $this->combineURL(self::GET_OPENID_URL, $keysArr);
-        $response = $this->get_contents($graph_url);
-
-        //--------检测错误是否发生
-        if(strpos($response, "callback") !== false){
-
-            $lpos = strpos($response, "(");
-            $rpos = strrpos($response, ")");
-            $response = substr($response, $lpos + 1, $rpos - $lpos -1);
-        }
-
-        $user = json_decode($response);
-        if(isset($user->error)){
-            echo '错误码：'.$user->error.'错误描述：'.$user->error_description;exit();
-//            $this->error->showError($user->error, $user->error_description);
-        }
-        //------记录openid
-//        session('openid',$user->openid);
-        return $user->openid;
-    }
-    /**
-     * combineURL
-     * 拼接url
-     * @param string $baseURL   基于的url
-     * @param array  $keysArr   参数列表数组
-     * @return string           返回拼接的url
-     */
-    public function combineURL($baseURL,$keysArr){
-        $combined = $baseURL."?";
-        $valueArr = array();
-
-        foreach($keysArr as $key => $val){
-            $valueArr[] = "$key=$val";
-        }
-
-        $keyStr = implode("&",$valueArr);
-        $combined .= ($keyStr);
-
-        return $combined;
     }
 
     /**
-     * get_contents
-     * 服务器通过get请求获得内容
-     * @param string $url       请求的url,拼接后的
-     * @return string           请求返回的内容
+     * @param Request $request
+     * @return mixed
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     * 首页
      */
-    public function get_contents($url){
-        if (ini_get("allow_url_fopen") == "1") {
-            $response = file_get_contents($url);
-        }else{
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-            curl_setopt($ch, CURLOPT_URL, $url);
-            $response =  curl_exec($ch);
-            curl_close($ch);
-        }
-
-        //-------请求为空
-        if(empty($response)){
-            $this->error->showError("50001");
-        }
-
-        return $response;
-    }
-
-
     public function index(Request $request)
     {
         $learn = new \app\admin\model\Learn();
