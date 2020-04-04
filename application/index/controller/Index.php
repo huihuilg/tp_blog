29982f35279c8e0270c70a1dee5eb3c79ab1b7a4
h <huihuilg@outlook.com>
2020-04-04 21:40:45 +0800

    qq登录

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,18 +9,171 @@ use think\Request;
 
 class Index extends Controller
 {
+
+    const VERSION = "2.0";
+    const GET_AUTH_CODE_URL = "https://graph.qq.com/oauth2.0/authorize";
+    const GET_ACCESS_TOKEN_URL = "https://graph.qq.com/oauth2.0/token";
+    const GET_OPENID_URL = "https://graph.qq.com/oauth2.0/me";
+    protected $recorder;
+    public $urlUtils;
+    protected $error;
+    private $APP_ID = 101544609;
+    private $APP_Key = '778dcbddc5147be3a33cc1f17a290d6d';
+    private $callback = 'http://sunny.huihuil.cn/index/index/qqaction';
+
+
 //    protected $middleware = [
 //        'Auth' 	=> ['except' 	=> ['index'] ],
 //        'Hello' => ['only' 	=> ['index'] ],
 //    ];
-  	
-  	public function test(Request $request)
+    public function test(Request $request)
     {
         header('location:http://www.baidu.com');
 //        header("HTTP/1.1 201 Created");
 //        file_put_contents("./test.txt",var_export($request->param(),true));
 //        exit();
     }
+
+    /**
+     * qq登录回调
+     */
+    public function qqLogin()
+    {
+        $appid = $this->APP_ID;
+        $callback = $this->callback;
+        $scope = 'get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr';
+        //-------生成唯一随机串防CSRF攻击
+        $state = md5(uniqid(rand(), TRUE));
+        $this->recorder['state'] = $state;
+
+        //-------构造请求参数列表
+        $keysArr = array(
+            "response_type" => "code",
+            "client_id" => $appid,
+            "redirect_uri" => $callback,
+            "state" => $state,
+            "scope" => $scope
+        );
+
+        $login_url =  $this->combineURL(self::GET_AUTH_CODE_URL, $keysArr);
+
+        header("Location:$login_url");
+
+    }
+    public function qqaction(Request $request)
+    {
+        $state = $this->recorder["state"];
+        //--------验证state防止CSRF攻击
+        if(!$state || $_GET['state'] != $state){
+            echo 'state 验证错误';exit();
+        }
+
+        //-------请求参数列表
+        $keysArr = array(
+            "grant_type" => "authorization_code",
+            "client_id" => $this->APP_ID,
+            "redirect_uri" => urlencode($this->callback),
+            "client_secret" => $this->APP_Key,
+            "code" => $_GET['code']
+        );
+
+        //------构造请求access_token的url
+        $token_url = $this->combineURL(self::GET_ACCESS_TOKEN_URL, $keysArr);
+        $response = $this->get_contents($token_url);
+
+        if(strpos($response, "callback") !== false){
+
+            $lpos = strpos($response, "(");
+            $rpos = strrpos($response, ")");
+            $response  = substr($response, $lpos + 1, $rpos - $lpos -1);
+            $msg = json_decode($response);
+
+            if(isset($msg->error)){
+                echo '错误码：'.$msg->error.'错误描述：'.$msg->error_description;exit();
+            }
+        }
+
+        $params = array();
+        parse_str($response, $params);
+
+        $this->recorder["access_token"] = $params["access_token"];
+        echo $params["access_token"];
+    }
+    public function get_openid(){
+
+        //-------请求参数列表
+        $keysArr = array(
+            "access_token" => $this->recorder["access_token"]
+        );
+
+        $graph_url = $this->combineURL(self::GET_OPENID_URL, $keysArr);
+        $response = $this->get_contents($graph_url);
+
+        //--------检测错误是否发生
+        if(strpos($response, "callback") !== false){
+
+            $lpos = strpos($response, "(");
+            $rpos = strrpos($response, ")");
+            $response = substr($response, $lpos + 1, $rpos - $lpos -1);
+        }
+
+        $user = json_decode($response);
+        if(isset($user->error)){
+            echo '错误码：'.$user->error.'错误描述：'.$user->error_description;exit();
+//            $this->error->showError($user->error, $user->error_description);
+        }
+
+        //------记录openid
+        $this->recorder["openid"] =  $user->openid;
+        echo $user->openid;
+    }
+    /**
+     * combineURL
+     * 拼接url
+     * @param string $baseURL   基于的url
+     * @param array  $keysArr   参数列表数组
+     * @return string           返回拼接的url
+     */
+    public function combineURL($baseURL,$keysArr){
+        $combined = $baseURL."?";
+        $valueArr = array();
+
+        foreach($keysArr as $key => $val){
+            $valueArr[] = "$key=$val";
+        }
+
+        $keyStr = implode("&",$valueArr);
+        $combined .= ($keyStr);
+
+        return $combined;
+    }
+
+    /**
+     * get_contents
+     * 服务器通过get请求获得内容
+     * @param string $url       请求的url,拼接后的
+     * @return string           请求返回的内容
+     */
+    public function get_contents($url){
+        if (ini_get("allow_url_fopen") == "1") {
+            $response = file_get_contents($url);
+        }else{
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($ch, CURLOPT_URL, $url);
+            $response =  curl_exec($ch);
+            curl_close($ch);
+        }
+
+        //-------请求为空
+        if(empty($response)){
+            $this->error->showError("50001");
+        }
+
+        return $response;
+    }
+
 
     public function index(Request $request)
     {
